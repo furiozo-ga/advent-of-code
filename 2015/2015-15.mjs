2396ddcf079b742e2f0a3c5950ddca63b711804d
furiozo <furiozo@gmail.com>
2022-12-27 01:47:19 +0200

    2015-15

---
+++ 2015/2015-15.mjs
@@ -0,0 +1,51 @@
+globalThis.l=console.log
+
+let inp1=`Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`
+let inp2=`Frosting: capacity 4, durability -2, flavor 0, texture 0, calories 5
+Candy: capacity 0, durability 5, flavor -1, texture 0, calories 8
+Butterscotch: capacity -1, durability 0, flavor 5, texture 0, calories 6
+Sugar: capacity 0, durability 0, flavor -2, texture 2, calories 1`
+
+let inp=inp2.split`\n`.map(r=>r.match(/-?\d+/g).map(Number))
+  , task=1
+
+
+l(opt([],100))
+task=2
+l(opt([],100))
+
+function opt(ls,left){
+    // l(ls,left)
+    if(ls.length==inp.length-1){
+        ls.push(left)
+
+        if(task==2){
+            let cal=0
+            for(let i=0 ; i< ls.length ;++i){
+                cal+=inp[i][4]*ls[i]
+            }
+            if(cal!=500) return 0
+        }
+
+        let sc=1
+        for(let q=0 ; q<4 ;++q){
+            let ssc=0
+            for(let i=0 ; i< ls.length ;++i){
+                ssc+=inp[i][q]*ls[i]
+                // l('ing',q,i,ssc)
+            }
+            if(ssc<0) ssc=0
+            sc*=ssc
+        }
+
+        // l('score',ls,sc)
+        return sc
+    }
+    let max=0
+    for(let q=0; q<=left ;++q){
+        let sub=opt([...ls,q],left-q)
+        if(sub>max) max=sub
+    }
+    return max
+}
